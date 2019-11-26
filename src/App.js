08f5e5c1e6dca8cecb9dc6ef0f0fa2a1ae1bda44
dannyGan <dannyakobus@yahoo.com>
2019-11-26 14:17:52 +0700

    samain

--- src/App.js
+++ src/App.js
@@ -4,38 +4,47 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 // import styled from "styled-components";
 
 import PrivateRoute from "./components/PrivateRoute";
-import LoginForm from "./components/Home";
+import Login from "./components/Home";
 import AuthButton from "./components/AuthButton";
 
 
 // import "./App.css";
 import NavBar from "./components/NavBar";
-import RegisterForm from "./components/RegisterForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import Todo from "./components/Todo";
 
 class App extends React.Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<div className="App">
-					<NavBar />
-				</div>
-				<Switch>
-					<Route exact path="/">
-						{/* <LoginForm /> */}
-            <RegisterForm/>
-					</Route>
-					<AuthButton />
-					<PrivateRoute path="/todo"></PrivateRoute>
-				</Switch>
-			</BrowserRouter>
-		);
-	}
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          {/* <Wrapper> */}
+          {/* <Header>React Todo</Header> */}
+          {/* <Todo /> */}
+          {/* </Wrapper> */}
+          {/* <Section dark>Dari Wrapper</Section> */}
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          {/* <Route path="/login"></Route> */}
+          {/* <Login /> */}
+          <AuthButton />
+          <PrivateRoute path="/todo">
+            {/* <Todo /> */}
+          </PrivateRoute>
+          {/* <PrivateRoute path="/github"></PrivateRoute> */}
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
 
+export default App;
 
 // export default class RegisterForm extends Component {
 // 	constructor(props) {
