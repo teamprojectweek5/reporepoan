f09b5b06ab6bdd8539dab2a3056f141a86b2d3b5
sharfinaega <ega.sharfina@gmail.com>
2019-11-26 10:22:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styled from "styled-components";
+
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Home";
+import AuthButton from "./components/AuthButton";
+
+// import "./App.css";
+import NavBar from "./components/NavBar";
+// import Todo from "./components/Todo";
+
+const Wrapper = styled.section`
+text-align: center
+padding: 4em;
+background: ${props => (props.dark ? "grey" : "yellow")}`;
+
+const Header = styled.h1`
+  text-align: center;
+  background: #000;
+  color: #fff;
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          {/* <Wrapper> */}
+          {/* <Header>React Todo</Header> */}
+          {/* <Todo /> */}
+          {/* </Wrapper> */}
+          {/* <Section dark>Dari Wrapper</Section> */}
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          {/* <Route path="/login"></Route> */}
+          {/* <Login /> */}
+          <AuthButton />
+          <PrivateRoute path="/todo">
+            <Wrapper>
+              <Header>React Todo</Header>
+              {/* <Todo /> */}
+            </Wrapper>
+            {/* <Todo /> */}
+          </PrivateRoute>
+          {/* <PrivateRoute path="/github"></PrivateRoute> */}
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
