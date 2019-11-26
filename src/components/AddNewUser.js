c1ee8176eb77305cad803bc8c12bc021efc6ed85
sharfinaega <ega.sharfina@gmail.com>
2019-11-26 14:06:32 +0700

    update

--- src/components/AddNewUser.js
+++ src/components/AddNewUser.js
@@ -1,7 +1,60 @@
 // import React, { Component } from "react";
-// import axios from 'axios' 
+// import axios from 'axios'
 
-
+class AddNewData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allData: [],
+      id: 0,
+      firstName: "",
+      lastName: "",
+      dateOfBirth: "",
+      gender: "",
+      email: "",
+      password: "",
+      status: false
+      //   isEdit: false
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(`https://cobacoba-hayepe.herokuapp.com/`)
+      .then(result => {
+        this.setState({
+          allData: result.data
+        });
+      })
+      .catch(error => console.log(error));
+  }
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  submitData = event => {
+    event.preventDefault();
+    const { allData, ...ambilSisanya } = this.state;
+    console.log(ambilSisanya);
+    axios
+      .post(`https://cobacoba-hayepe.herokuapp.com/`, ambilSisanya)
+      .then(result => {
+        console.log(result);
+        this.setState({
+          firstName: "",
+          lastName: "",
+          dateOfBirth: "",
+          gender: "",
+          email: "",
+          password: "",
+          allData: result.data.allData
+        });
+      })
+      .catch(error => console.log(error));
+    // console.log(this.state);
+  };
+}
 
 // class AddNewUser extends Component {
 //   render() {
@@ -19,7 +72,7 @@
 //           .then(result => console.log(result))
 //           .catch(error => console.log(error));
 //       };
-      
+
 //     return <div>
 
 //     </div>;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,62 +3,8 @@ import { Button, Form, Grid, Header, Image, Message, Segment } from "semantic-ui
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { fakeAuth } from "../helpers/fakeAuth";
 import logo from "./images.png";
-import axios from "axios";
+// import axios from "axios";
 
-class AddNewData extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allData: [],
-      id: 0,
-      firstName: "",
-      lastName: "",
-      dateOfBirth: "",
-      gender: "",
-      email: "",
-      password: "",
-      status: false
-      //   isEdit: false
-    };
-  }
-  componentDidMount() {
-    axios
-      .get(`https://cobacoba-hayepe.herokuapp.com/`)
-      .then(result => {
-        this.setState({
-          allData: result.data
-        });
-      })
-      .catch(error => console.log(error));
-  }
-  handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-  submitData = event => {
-    event.preventDefault();
-    const { allData, ...ambilSisanya } = this.state;
-    console.log(ambilSisanya);
-    axios
-      .post(`https://cobacoba-hayepe.herokuapp.com/`, ambilSisanya)
-      .then(result => {
-        console.log(result);
-        this.setState({
-          firstName: "",
-          lastName: "",
-          dateOfBirth: "",
-          gender: "",
-          email: "",
-          password: "",
-          allData: result.data.allData
-        });
-      })
-      .catch(error => console.log(error));
-    // console.log(this.state);
-  };
-}
 const LoginForm = () => {
   let history = useHistory();
   let location = useLocation();
